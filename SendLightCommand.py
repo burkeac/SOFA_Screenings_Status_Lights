4b39ded05ca90decf6d52c903d016a461901ba89
Adam Burke <adam.burke603@gmail.com>
2020-01-21 23:34:29 -0500

    Added command line interface with arguments.

--- SendLightCommand.py
+++ SendLightCommand.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
 
+
+import argparse
 import socket
-import sys
 
-# IP of serving computer
-HOST = '192.168.86.20' #'127.0.0.1' #'169.254.207.114'
+parser = argparse.ArgumentParser(description='Send commands to light strips', usage="SendLightCommand HostAdress LightColor [options]")
+
+parser.add_argument('HostAddress', action='store', type=str)
+parser.add_argument('LightColor', choices=['red', 'green', 'blue'])
+
+parser.add_argument('--addAddress', '-A', action='append')
+parser.add_argument('--SetPort', '-P', action="store")
+parser.add_argument('--Verbose', '-V', action='store_true')
+
+args = parser.parse_args()
+
+# combine the required and optional IP addresses into a single string
+ipAddresses = args.addAddress
+ipAddresses.insert(0, str(args.HostAddress))
+
+if args.Verbose:
+    if      ipAddresses.length > 1: print("Connecting to hosts:")
+    else:   print("Connecting to host:")
+    for HOST in ipAddresses:
+        print(HOST)
+
 PORT = 1234
+if args.SetPort is not None: PORT = int(args.SetPort)
 
-# Connect to TCP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+print("Connecting to host using port: " + str(PORT))
 
-# format message from command line arguments
-arguments = sys.argv
-message = ''
-i = 1
-while i < len(arguments):
-    message += arguments[i] + ' '
-    i += 1
-message = message[:-1]
+message = str(args.LightColor)
 
-# send the message
-s.send(bytes(str(message), 'ascii'))
-print("message sent: " + message)
+if args.Verbose: print("Sending message: " + message)
+
+
+for HOST in ipAddresses:
+    # Connect to TCP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    # send the message
+    s.send(bytes(str(message), 'ascii'))
+    print("message sent: " + message)
